20e03dcc1ebe94ecc2a59c821ef845e670338e72
Morgan Browne <morgan.browne@keyin.com>
2025-02-27 14:37:57 -0330

    added airport class

--- src/main/java/com/keyin/domain/Airport.java
+++ src/main/java/com/keyin/domain/Airport.java
@@ -1,4 +1,37 @@
 package com.keyin.domain;
 
 public class Airport {
+    private String id;
+    private String name;
+    private String location;
+
+
+    public Airport() {}
+    public Airport(String id, String name, String location) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
